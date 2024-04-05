8eb5c2e243c835976dcd35e7266bcca6cab4d43b
ADIITJ <b22ai045@iitj.ac.in>
2024-04-05 20:18:24 +0530

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import wikipedia
-
+# Function to search Wikipedia and extract content from the top 5 results
 # Function to search Wikipedia and extract content from the top 5 results
 def search_and_extract(query):
     url = f"https://en.wikipedia.org/w/index.php?search={query.replace(' ', '+')}&title=Special:Search&profile=advanced&fulltext=1&ns0=1"
@@ -10,21 +9,34 @@ def search_and_extract(query):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         links = [div.a['href'] for div in soup.find_all('div', class_='mw-search-result-heading')][:3]
+        if len(links)==0:
+            print('No results found! Try again')
+            return
         for url in links:
             title = url.split("/")[-1]
             print(title)
             extract_content(title, query)
+    else:
+        print("Error in getting webpage")
+        
 
-# Function to extract content from a Wikipedia page and store it in a text file
 def extract_content(title, query):
     try:
-        page = wikipedia.page(title)
-        content = page.content
-        with open("all_content.txt", "a", encoding="utf-8") as f:
-            f.write(content)
-            f.write("\n")
-        print(f"Content extracted and saved to all_content.txt")
-        extract_chunks(content, query)
+        wikipedia_url = f"https://en.wikipedia.org/wiki/{title}"
+        response = requests.get(wikipedia_url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            content_paragraphs = soup.select("p")
+            content = "\n".join([paragraph.get_text() for paragraph in content_paragraphs])
+
+            with open("all_content.txt", "a", encoding="utf-8") as f:
+                f.write(content)
+                f.write("\n")
+            print(f"Content extracted and saved to all_content.txt")
+            extract_chunks(content, query)
+        else:
+            print(f"Failed to fetch Wikipedia page for '{title}'. Status code: {response.status_code}")
+
     except Exception as e:
         print("Error:", e)
 
@@ -47,3 +59,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
